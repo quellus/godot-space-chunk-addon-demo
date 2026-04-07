0d46360da1d1b483b247b05e31452b37f075a279
quellus <quellus7@gmail.com>
2026-04-06 20:30:08 -0400

    process queue on each frame adding and deleting new chunks as the player moves

--- scripts/space.gd
+++ scripts/space.gd
@@ -1,44 +1,80 @@
 class_name Space extends Node3D
 
 @export var space_chunk_scene: PackedScene
-@export var chunk_radius: int = 3
 @export var focus: Node3D
+@export var chunk_radius: int = 3
+@export var chunk_size: int = 16
 
-var active_chunks: Array[SpaceChunk] = []
+var focus_chunk_coord: Vector3i:
+	set(value):
+		if value != focus_chunk_coord:
+			focus_chunk_coord = value
+			_add_chunks_to_gen_queue()
+			_add_chunks_to_delete_queue()
+			print_debug("Active chunks: ", active_chunks.size(), " Del queue: ", chunk_delete_queue.size(), " Gen queue: ", chunk_generate_queue.size())
+
+var active_chunks: Dictionary[Vector3i, SpaceChunk] = {}
 var chunk_delete_queue: Array[SpaceChunk] = []
-var chunk_generate_queue: Array[SpaceChunk] = []
+var chunk_generate_queue: Array[Vector3i] = []
 
-var chunk_process_state: ChunkProcessState = ChunkProcessState.CHECK
+var chunk_process_state: ChunkProcessState = ChunkProcessState.GENERATE
 enum ChunkProcessState {
 	CHECK,
 	DELETE,
 	GENERATE
 }
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var focus_chunk_coord = Vector3i(focus.global_position / space_chunk_scene.CHUNK_SIZE)
-	if chunk_process_state == ChunkProcessState.CHECK:
-		_add_chunks_to_gen_queue(focus_chunk_coord)
-		_add_chunks_to_delete_queue(focus_chunk_coord)
-		chunk_process_state = ChunkProcessState.DELETE
-	if chunk_process_state == ChunkProcessState.DELETE:
-		_pop_delete_queue()
-		chunk_process_state = ChunkProcessState.GENERATE
-	if chunk_process_state == ChunkProcessState.GENERATE:
-		_pop_gen_queue()
-		chunk_process_state = ChunkProcessState.CHECK
+func _process(_delta: float) -> void:
+	if is_instance_valid(focus):
+		focus_chunk_coord = Vector3i(focus.global_position / chunk_size)
+		if chunk_process_state == ChunkProcessState.DELETE:
+			if chunk_delete_queue.size() > 0:
+				_pop_delete_queue()
+			chunk_process_state = ChunkProcessState.GENERATE
+		if chunk_process_state == ChunkProcessState.GENERATE:
+			if chunk_generate_queue.size() > 0:
+				_pop_gen_queue()
+			chunk_process_state = ChunkProcessState.DELETE
+	else:
+		push_error("Space object does not have focus")
+
+
+func _add_chunks_to_gen_queue():
+	chunk_generate_queue.clear()
 	for x in range(-chunk_radius, chunk_radius):
 		for y in range(-chunk_radius, chunk_radius):
 			for z in range(-chunk_radius, chunk_radius):
-				_generate_chunk(Vector3i(x, y, z))
+				var chunk_coord: Vector3i = Vector3i(x,y,z) + focus_chunk_coord
+				if not is_instance_valid(chunk_coord):
+					if chunk_coord not in chunk_generate_queue:
+						chunk_generate_queue.append(chunk_coord)
 
 
+func _add_chunks_to_delete_queue():
+	chunk_delete_queue.clear()
+	for chunk_coord in active_chunks:
+		var distance = chunk_coord - focus_chunk_coord
+		if abs(distance.x) > chunk_radius or abs(distance.y) > chunk_radius or abs(distance.z) > chunk_radius:
+			chunk_delete_queue.append(active_chunks[chunk_coord])
+
+
+func _pop_delete_queue():
+	var to_delete = chunk_delete_queue.pop_front()
+	if is_instance_valid(to_delete) and is_instance_valid(active_chunks[to_delete.chunk_coord]):
+		active_chunks.erase(to_delete.chunk_coord)
+		to_delete.queue_free()
+
+
+func _pop_gen_queue():
+	var chunk_coord: Vector3i = chunk_generate_queue.pop_front()
+	if not chunk_coord in active_chunks or not is_instance_valid(active_chunks[chunk_coord]):
+		_generate_chunk(chunk_coord)
 
 
 func _generate_chunk(chunk_coord: Vector3i):
 	var space_chunk = space_chunk_scene.instantiate()
 	add_child(space_chunk)
 	space_chunk.chunk_coord = chunk_coord
+	space_chunk.chunk_size = chunk_size
 	space_chunk.generate()
-	active_chunks.append(space_chunk)
+	active_chunks[chunk_coord] = space_chunk
--- scripts/space_chunk.gd
+++ scripts/space_chunk.gd
@@ -1,21 +1,21 @@
 class_name SpaceChunk extends Node3D
 
-const CHUNK_SIZE: int = 16
+var chunk_size: int = 16
 const SPACE_OBJECT = preload("res://scenes/space_object.tscn")
 
 var chunk_coord: Vector3i = Vector3i.ZERO:
 	set(value):
 		chunk_coord = value
-		global_position = Vector3(value) * CHUNK_SIZE
+		global_position = Vector3(value) * chunk_size
 
 var fast_noise_lite: FastNoiseLite = load("res://resources/new_fast_noise_lite.tres")
 
 var rng = RandomNumberGenerator.new()
 
 func generate() -> void:
-	for x in range(-CHUNK_SIZE, CHUNK_SIZE):
-		for y in range(-CHUNK_SIZE, CHUNK_SIZE):
-			for z in range(-CHUNK_SIZE, CHUNK_SIZE):
+	for x in range(-chunk_size, chunk_size):
+		for y in range(-chunk_size, chunk_size):
+			for z in range(-chunk_size, chunk_size):
 				var global_coords: Vector3 = Vector3(global_position.x + x,global_position.y + y,global_position.z + z)
 				var noise_val = fast_noise_lite.get_noise_3dv(global_coords)
 				if  noise_val > 0.67:
